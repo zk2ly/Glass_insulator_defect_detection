6e116887f61dac34b7818bf5d300f26165a259ad
zk2ly <56818387+zk2ly@users.noreply.github.com>
2020-12-14 17:12:55 +0800

    Delete gen_empty_label.py

--- code/gen_empty_label.py
+++
@@ -1,9 +0,0 @@
-import os
-
-imgs = os.listdir('/home/zk/darknet/insulator_data/train/JPEGImages/')
-
-for img in imgs:
-    label_path = '/home/zk/darknet/insulator_data/train/labels/' + img.split('.')[0] + '.txt'
-    if not os.path.exists(label_path):
-        f = open(label_path, 'w')
-        f.close()
